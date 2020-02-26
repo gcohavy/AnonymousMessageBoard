c4d9423291944336a9c3e722988a15ccc3793ffe
Glitch (seed-cyclone-dgmbnt7dl) <none>
2020-02-26 01:26:47 +0000

    💼🐊 Checkpoint ./controllers/replyHandler.js:3976953/515

--- controllers/replyHandler.js
+++ controllers/replyHandler.js
@@ -5,16 +5,23 @@ var db_connection_string = process.env.DB;
 function ReplyHandler () {
   this.addReply = function(req, res) {
     var board = req.params.board;
+    var id = req.body.thread_id;
+    var bumped_on = new Date();
     var save = {
       text: req.body.text,
       delete_password: req.body.delete_password,
-      thread_id: req.body.thread_id
+      created_on: new Date(),
+      reported: false
     };
     mongoClient.connect(db_connection_string, {useUnifiedTopology: true}, (err, client)=> {
       if(err) console.log(err);
       var db = client.db('test');
       var collection = db.collection(board);
-      collection.findOneAndUpdate({_id: req.body.thread_id})
+      collection.findOneAndUpdate({_id: id},{bumped_on: bumped_on, $addToSet: {replies: save}}, {returnNewDocument: true},(err, ret) =>{
+        if(err) console.log(err);
+        console.log(ret);
+        res.redirect('/b/'+ board + '/' + id);
+      })
     })
     
   }
